b7df8664db9dfed6db84ff1a64a4498612c68aec
Nikita Smorschok <nsmorschok@gmail.ru>
2022-05-12 22:04:03 +0300

    Streams

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,19 @@
 package com.company;
 import java.util.*;
 
+
 public class Main {
 
     public static void main(String[] args) {
-
-        int element = 10;
-        ArrayList<Integer> integer = new ArrayList<>(element);
-        for (int i = 0; i < element; i++) {
-            int a = (int) (Math.random() * 10 +7);
-            integer.add(a);
-        }
-        System.out.println("Random element insert"+integer);
-        System.out.println("sheet contains elements: "+ (long) integer.size());
-        System.out.println("duplicate delete: "+integer.stream().distinct().toList());
+        List <Integer> numbers = new Random().ints(10,7,17).boxed().toList();
+        System.out.println("Random element insert"+numbers);
+        System.out.println("sheet contains elements: "+ (long) numbers.size());
+        System.out.println("duplicate delete: "+numbers.stream().distinct().toList());
         for (String s : Arrays.asList(
-                "multiplication for 2: " + integer.stream().map((a) -> a * 2).toList(),
-                "after sorted 4 first element: " + integer.stream().sorted().distinct().limit(4).toList(),
-                "Maths arithmetic operation: " + integer.stream().mapToInt((a) -> Integer.parseInt(String.valueOf(a))).average(),
-                "even numbers 7 - 17: " + integer.stream().filter(o -> o % 2 == 0).toList())) {
+                "multiplication for 2: " + numbers.stream().map((a) -> a * 2).toList(),
+                "after sorted 4 first element: " +numbers.stream().sorted().distinct().limit(4).toList(),
+                "Maths arithmetic operation: " + numbers.stream().mapToInt( (a) -> a ).average(),
+                "even numbers 7 - 17: " + numbers.stream().filter(o -> o % 2 == 0).toList())) {
             System.out.println(s);
         }
     }
